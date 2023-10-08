983dfb50ec0a349d9842950ba348b88869faa945
buzdev <kanashkaze02@gmail.com>
2023-10-08 21:53:54 +0700

    subarrays equals k <positive & negative element>

--- Arrays/Subarray_Sum_Equals_K.cpp
+++ Arrays/Subarray_Sum_Equals_K.cpp
@@ -13,22 +13,41 @@ int longestSubarrayWithSumK(vector<int> a, long long k)
     for (auto c : a)
     {
         sum += c;
-        prefix_sum[sum] = i;
-        i++;
-        if (prefix_sum[sum - k] != 0)
+        std::cout << "Prefix sum untill " << i << ": " << sum << std::endl;
+        std::cout << "remain sum : " << sum - k << std::endl;
+        if (sum == k)
         {
-            int length = i - prefix_sum[sum - k] - 1;
+            maxlength = std::max(i + 1, maxlength);
+        }
+        if (prefix_sum.find(sum - k) != prefix_sum.end())
+        {
+            int length = i - prefix_sum[sum - k];
+            std::cout << "Length: " << length << std::endl;
             if (length > maxlength)
             {
                 maxlength = length;
             }
         }
+        if (prefix_sum.find(sum) == prefix_sum.end())
+        {
+            prefix_sum[sum] = i;
+        }
+        i++;
     }
     return maxlength;
 }
 int main()
 {
-    vector<int> v = {1, 1, 0, 1, 1};
-    int k = 2;
-    std::cout << "Max Length = " << longestSubarrayWithSumK(v, k) << std::endl;
+    vector<int> v = {2, 3, 5};
+    int k = 5;
+    int res = longestSubarrayWithSumK(v, k);
+    std::cout << "Max Length = " << res << std::endl;
+
+    std::unordered_map<int, int> umap;
+    umap[0] = 1;
+    umap[0] = 2;
+    for (auto m : umap)
+    {
+        std::cout << "first: " << m.first << ", second:  " << m.second << std::endl;
+    }
 }
